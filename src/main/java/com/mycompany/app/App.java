5cd199cbcea1caac56c08a14b1b39f67b134ec45
kbekleyen <kaanmbekleyen@gmail.com>
2024-03-12 18:31:33 +0300

    Initializing Project

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+
+    static String process(Integer[] box1, Integer[] box2, int mode, char displayMode){
+        return "";
+    }
+}
